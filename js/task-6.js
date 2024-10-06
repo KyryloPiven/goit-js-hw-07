01e18952c397a1c8489fc31fae6c25ddcd991e1c
Kyrylo Piven <pivenkirill24@gmail.com>
2024-10-06 14:42:19 +0100

    saveitog

--- js/task-6.js
+++ js/task-6.js
@@ -3,3 +3,35 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+const input = document.querySelector("#controls input");
+const createBtn = document.querySelector("[data-create]");
+const destroyBtn = document.querySelector("[data-destroy]");
+const boxesContainer = document.getElementById("boxes");
+createBtn.addEventListener("click", () => {
+  const amount = parseInt(input.value);
+  if (amount >= 1 && amount <= 100) {
+    createBoxes(amount);
+    input.value = "";
+  } else {
+    alert("Please enter a number between 1 and 100.");
+  }
+});
+destroyBtn.addEventListener("click", destroyBoxes);
+function createBoxes(amount) {
+  destroyBoxes();
+  const elements = [];
+  let size = 30;
+  for (let i = 0; i < amount; i++) {
+    const div = document.createElement("div");
+    div.style.width = `${size}px`;
+    div.style.height = `${size}px`;
+    div.style.backgroundColor = getRandomHexColor();
+    div.style.margin = "5px";
+    elements.push(div);
+    size += 10;
+  }
+  boxesContainer.append(...elements);
+}
+function destroyBoxes() {
+  boxesContainer.innerHTML = "";
+}
